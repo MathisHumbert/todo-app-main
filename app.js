a7c4bf9b9f146ddebbf8ec5415204da1cece8d9c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-03 19:46:19 +0200

    some JS added

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const form = document.querySelector('.todo-form');
 const input = document.querySelector('.todo-input');
 const list = document.querySelector('.dynamic-list');
 const count = document.querySelector('.count-list');
+const deleteBtn = document.querySelectorAll('.cross');
+const checkBtn = document.querySelectorAll('.circle');
 
 let amount = 0;
 
 // add a new list to do
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  amount++;
   let value = input.value;
   let content = document.createElement('div');
   content.classList.add('single-list', 'active');
+  content.setAttribute('data-id', amount);
   content.innerHTML = `
               <div class="todo-info">
                 <div class="circle">
@@ -18,11 +22,11 @@ form.addEventListener('submit', (e) => {
                 </div>
                 <p>${value}</p>
               </div>
-              <img src="./images/icon-cross.svg" alt="" />
+              <img src="./images/icon-cross.svg" class="cross" />
   `;
   list.appendChild(content);
   input.value = '';
-  amount++;
+
   countList(amount);
 });
 
@@ -32,10 +36,38 @@ const countList = () => {
 };
 
 // delete the list targeted
-// foreach des btn avec un data id
+deleteBtn.forEach((btn) => {
+  console.log(btn);
+  let id = btn.parentElement.dataset.id;
+  btn.addEventListener('click', (e) => {
+    if (e.target.parentElement.dataset.id === id) {
+      e.target.parentElement.remove();
+    }
+  });
+});
 
 // completed the list targeted
-// foreach des circles avec un data id
+checkBtn.forEach((btn) => {
+  console.log(btn);
+  let id = btn.parentElement.parentElement.dataset.id;
+  btn.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target.classList.contains('check')) {
+      e.target.parentElement.parentElement.parentElement.classList.remove(
+        'completed'
+      );
+    }
+    if (e.target.parentElement.parentElement.dataset.id === id) {
+      if (
+        e.target.parentElement.parentElement.classList.contains('completed')
+      ) {
+        e.target.parentElement.parentElement.classList.remove('completed');
+      } else {
+        e.target.parentElement.parentElement.classList.add('completed');
+      }
+    }
+  });
+});
 
 // clear completed
 // enlever tous les completed des btns for each des circles
