e8bb6567081fa4b799b1eef486de1d6cc85e45a7
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-05 19:01:26 +0200

    some js added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // getElement
 const form = document.querySelector('.todo-form');
 const input = document.querySelector('.todo-input');
-const list = document.querySelector('.dynamic-list');
 const count = document.querySelector('.count-list');
+const clearAll = document.querySelector('.clear-all');
+const list = document.querySelector('.dynamic-list');
 
 // inital amount of list
 let amount = 0;
@@ -13,35 +14,44 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   amount++;
   let value = input.value;
+
+  // display the new list
   singleList(value, amount);
 
   // get new element
   const deleteBtn = document.querySelectorAll('.cross');
   const checkBtn = document.querySelectorAll('.circle');
+  const uniqueList = document.querySelectorAll('.single-list');
 
   // delete the list targeted
   deleteBtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      let id = e.target.parentElement.dataset.id;
-      let deleteditem = items.filter(
-        item,
-        (index) => item[index].amount === id
-      );
-      console.log(deleteditem);
-    });
+    btn.addEventListener('click', (e) => {});
   });
   // complete the list tageted
   checkBtn.forEach((btn) => {
     const id = btn.parentElement.parentElement.dataset.id;
-
     btn.addEventListener('click', (e) => {
-      if (e.target.parentElement.parentElement.dataset.id === id) {
-        e.target.parentElement.parentElement.classList.toggle('completed');
+      if (e.target.classList.contains('check')) {
+        e.target.parentElement.parentElement.parentElement.classList.remove(
+          'completed'
+        );
+      } else {
+        if (e.target.parentElement.parentElement.dataset.id === id) {
+          e.target.parentElement.parentElement.classList.toggle('completed');
+        }
       }
     });
   });
+
   // update number of items
   countList(amount);
+
+  // cleat all completed
+  clearAll.addEventListener('click', () => {
+    uniqueList.forEach((list) => {
+      list.classList.remove('completed');
+    });
+  });
 });
 
 // create unique item
@@ -83,10 +93,6 @@ const countList = () => {
 // filter les .single-list qui n'ont pas de class completed
 // soit en utilisant un filter mais il faut que mes items soit dans un tableau ou avec un if contains
 
-// completed
-// filter avec class completed
-
-// bug completed a résoudre en cours
 // click event sur le container des 3
 // if(textcontent === 'all'), display = "block"
 // if(textcontent === 'completed'), display all !classlist.contains display = "none"
