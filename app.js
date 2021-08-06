38aa1d51d784bf05b3c56bd8cf71b21230fbbab2
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-06 10:45:07 +0200

    js changes

--- app.js
+++ app.js
@@ -7,26 +7,24 @@ const list = document.querySelector('.dynamic-list');
 
 // inital amount of list
 let amount = 0;
-let items = [];
 
-// add a new list to do
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   amount++;
   let value = input.value;
 
-  // display the new list
-  singleList(value, amount);
+  // create the new list
 
   // get new element
-  const deleteBtn = document.querySelectorAll('.cross');
-  const checkBtn = document.querySelectorAll('.circle');
-  const uniqueList = document.querySelectorAll('.single-list');
+  const deleteBtn = document.querySelector('.cross');
+  const checkBtn = document.querySelector('.circle');
+  const uniqueList = document.querySelector('.single-list');
 
   // delete the list targeted
   deleteBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {});
   });
+
   // complete the list tageted
   checkBtn.forEach((btn) => {
     const id = btn.parentElement.parentElement.dataset.id;
@@ -52,39 +50,16 @@ form.addEventListener('submit', (e) => {
       list.classList.remove('completed');
     });
   });
+
+  // display the final list
 });
-
-// create unique item
-const singleList = (value, amount) => {
-  let item = { value, amount, state: 'active' };
-  items.push(item);
-
-  let allItems = items
-    .map((item) => {
-      return `
-    <div class="single-list active" data-id="${item.amount}">
-              <div class="todo-info">
-                <div class="circle">
-                  <i class="fas fa-check check"></i>
-                </div>
-                <p>${item.value}</p>
-              </div>
-              <img src="./images/icon-cross.svg" class="cross" />
-            </div>
-    `;
-    })
-    .join('');
-  list.innerHTML = allItems;
-  input.value = '';
-};
 
 // count number of list
 const countList = () => {
   count.textContent = `${amount} items left`;
 };
 
-// clear completed
-// enlever tous les completed des btns for each des circles
+// refaire la partie de création de la list et afficher la list en fin
 
 // all
 // display all
